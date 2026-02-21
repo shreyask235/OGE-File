f1fe9149d882484eaebf4d6aa126f94f2387718c
Shreyas Kundaragi <shreyaskundaragi@gmail.com>
2026-02-20 19:57:07 -0600

    OGE file uploaded; struct coded for the column data for the OGE file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,46 @@
-﻿namespace reading_oge_data;
+﻿namespace CSharpAssignments;
+using System;
 
-class Program
-{
-    static void Main(string[] args)
+    public struct ColumnData
     {
-        Console.WriteLine("Hello, World!");
+        public string DisplayName;
+        public string FirstName;
+        public string LastName;
+        public string WorkEmail;
+        public string IdentityId;
+        public string AccessDisplayName;
+        public string Uid;
+        public string AccessDescription;
+        public bool   IsManager;
+        public string Department;
+        public string JobTitle;
+        public string AccessType;
+        public string AccessSourceName;
     }
-}
+
+
+    class Program
+    {
+        private Dictionary<string, List<ColumnData>> students = new Dictionary<string, List<ColumnData>>();
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.ReadCSV("Francis Tuttle Identities_Basic.xlsx");
+        }
+
+
+        void ReadCSV(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line = reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    
+                }
+            }
+        }
+    }
+
